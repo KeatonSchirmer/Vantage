ec049423071e73c76e9eccee43d6e06fc47ca790
Keaton Schirmer <keasch1589@gmail.com>
2025-05-30 14:52:18 -0500

    Commit

--- app.py
+++ app.py
@@ -714,13 +714,8 @@ def history():
         app.logger.error(f'Error in /message route: {e}')
         return 'An error occured while fetching messages'
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
