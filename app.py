9037c24dc30a0a89fc5c6aedf0e056b28463ac76
Keaton Schirmer <keasch1589@gmail.com>
2025-05-30 15:04:53 -0500

    environment changes

--- app.py
+++ app.py
@@ -22,12 +22,12 @@ app = Flask(__name__)
 
 
 #* DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user_v76t_user:btcqKvJJQKOB2JYNo7fHkNrl3FpSuNp5@dpg-d0rv1di4d50c73b3drb0-a/user_v76t'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
-app.secret_key = 'schirmer_czubaj'
+app.secret_key = os.environ.get('SECRET_KEY')
 
 
 #* Cache
@@ -40,8 +40,8 @@ cache = Cache(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'keasch1589@gmail.com'
-app.config['MAIL_PASSWORD'] = 'otit atoy kjjn knta'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 mail = Mail(app)
 
 s = URLSafeTimedSerializer(app.secret_key)
